9a6b6808074ce680ba1bc4803c43c028b2825c54
Patrick Lehman <54288924+plehman2000@users.noreply.github.com>
2022-10-12 14:27:19 -0400

    added openai stuff

--- arguio/src/components/Argboxes/Argboxes.tsx
+++ arguio/src/components/Argboxes/Argboxes.tsx
@@ -10,10 +10,7 @@ const Argboxes: React.FC<ArgboxesProps> = ({ posA, posB }) => {
     const [buttonClicked, setButtonClicked] = useState(false);
     // const posA = "Ukraine is a country that is in transition and is working to reform its economy and political system. Second, Ukraine has a large population and astrategic location, making it important to stability in the region. Third, Ukraine has been avictim of Russian aggression, and giving aid will help to offset Russian influence. Finally, giving aid to Ukraine will help to promote democracy and human rights in the country."
     // const posB = "There are many reasons why giving aid to Ukraine is unreasonable. First, Ukraine is a country with a history of corruption, and it is not clear that the aid would be used effectively.Second, Ukraine is in a difficult financial situation, and it is not clear that it wouldbe able to repay the aid. Third, there is a risk that the aidwould be used to finance military action against Russia, which could lead to a wider conflict."
-    const numEntities_posA = 4;
-    const numEntities_posB = 4;
-    var posA_entityColumns = 0;
-    var posB_entityColumns = 0;
+
 
 
     return (
@@ -38,7 +35,7 @@ const Argboxes: React.FC<ArgboxesProps> = ({ posA, posB }) => {
                     <Center><Badge borderRadius={5} px='2' colorScheme='teal' fontSize='1.25em' marginBottom={2}>
                         Entities
                     </Badge></Center>
-                    
+
                     <Flex w ="100%" flexWrap="wrap" justifyContent="center">
                             <EntityBox text="Entity Name"></EntityBox>
                             <EntityBox text="Entity Name"></EntityBox>
