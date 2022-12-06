dd8ae5d8b913e083939d4f68147579efb10724e1
Liam Sy <liamrs222@gmail.com>
2022-12-05 19:02:11 -0500

    added homepage

--- arguio/src/components/Navbar/Navbar.tsx
+++ arguio/src/components/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ import { getAuth } from "firebase/auth" // New import
 
 
 const firebaseConfig = {
-    apiKey: "",
+    apiKey: "AIzaSyAa9Z61heACTTYjfSfGb-7wJZS71ZtSgXo",
     authDomain: "arguio-3dea1.firebaseapp.com",
     databaseURL: "https://arguio-3dea1-default-rtdb.firebaseio.com",
     projectId: "arguio-3dea1",
@@ -124,9 +124,9 @@ const Navbar: React.FC<NavbarProps> = () => {
 
     return ( 
             <Flex bg="teal.100" margin="auto" width="100%" height="75"  mt={2} p={4} align="center"> 
-           <Icon as={BiGitRepoForked} w="75px" h = "50px"/>
+           <Icon as={BiGitRepoForked} w="75px" h = "50px" onClick={() => { router.push("/homepage") }}/>
            
-           <Box   marginBottom={2} p={1} color="white" fontSize='2em' onClick={() => { router.push("/") }}>
+           <Box   marginBottom={2} p={1} color="black" fontSize='2em' fontWeight='bold' onClick={() => { router.push("/") }}>
                 argu.io
             </Box>
             <Button leftIcon={<BiGitRepoForked />} marginLeft="20px" bg="white" color="grey" variant='solid' size='lg' onClick={() => { router.push("/") }}>
